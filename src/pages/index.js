6330b20075a00361bcf2677aa54532bbbd149bae
kimboj23 <kimboj838@gmail.com>
2025-03-07 18:12:38 +0800

    Revert "minor edits"
    This reverts commit 37b9ff8c76dda601ed3803fd2a9b081d377cdf15.

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <Layout
       title={`Mại vô ${siteConfig.title}`}
-      description="Bên nhau học hành">
+      description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
